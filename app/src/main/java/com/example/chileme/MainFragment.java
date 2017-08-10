5ca3646591f60633ce6e539b1f17701a16cd9af0
fwssnbb <740677997@qq.com>
2017-08-10 14:09:53 +0800

    添加fragment样式

--- app/src/main/java/com/example/chileme/MainFragment.java
+++ app/src/main/java/com/example/chileme/MainFragment.java
@@ -7,7 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.ViewInject;
@@ -19,13 +19,13 @@ import okhttp3.Request;
 public class MainFragment extends FragmentActivity {
 
     @ViewInject(R.id.firstpage_frag)
-    private TextView firstpage_frag;
+    private ImageView firstpage_frag;
     @ViewInject(R.id.discover_frag)
-    private TextView discover_frag;
+    private ImageView discover_frag;
     @ViewInject(R.id.order_frag)
-    private TextView order_frag;
+    private ImageView order_frag;
     @ViewInject(R.id.personal_frag)
-    private TextView personal_frag;
+    private ImageView personal_frag;
     private Fragment contentFragment;
     public static  FragmentManager fragmentManager;
     private FragmentTransaction transaction;
@@ -50,23 +50,20 @@ public class MainFragment extends FragmentActivity {
         switch (flag){
             case 1:
                 init=new FirstPageFragment();
-                firstpage_frag.setBackgroundColor(0Xff4169E1);
-                firstpage_frag.setTextColor(0xff000000);
+                firstpage_frag.setImageResource(R.drawable.x102);
+
                 break;
             case 2:
                 init=new DiscoverFragment();
-                discover_frag.setBackgroundColor(0Xff4169E1);
-                discover_frag.setTextColor(0xff000000);
+                discover_frag.setImageResource(R.drawable.x202);
                 break;
             case 3:
                 init=new OrderFragment();
-                order_frag.setBackgroundColor(0Xff4169E1);
-                order_frag.setTextColor(0xff000000);
+                order_frag.setImageResource(R.drawable.x302);
                 break;
             case 4:
                 init=new PersonalFragment();
-                personal_frag.setBackgroundColor(0Xff4169E1);
-                personal_frag.setTextColor(0xff000000);
+                personal_frag.setImageResource(R.drawable.x402);
                 break;
             default:
                 break;
@@ -81,32 +78,36 @@ public class MainFragment extends FragmentActivity {
 
         @Override
         public void onClick(View v) {
-            firstpage_frag.setBackgroundColor(0xffffffff);
-            firstpage_frag.setTextColor(0Xff4169E1);
-            discover_frag.setBackgroundColor(0Xffffffff);
-            discover_frag.setTextColor(0Xff4169E1);
-            order_frag.setBackgroundColor(0xffffffff);
-            order_frag.setTextColor(0Xff4169E1);
-            personal_frag.setBackgroundColor(0xffffffff);
-            personal_frag.setTextColor(0Xff4169E1);
-            TextView textview = (TextView) v;
-            textview.setBackgroundColor(0Xff4169E1);
-            textview.setTextColor(0xff000000);
+            firstpage_frag.setImageResource(R.drawable.x101);
+            discover_frag.setImageResource(R.drawable.x201);
+
+            order_frag.setImageResource(R.drawable.x301);
+
+            personal_frag.setImageResource(R.drawable.x401);
+
+//            TextView textview = (TextView) v;
+//            textview.setBackgroundColor(0Xff4169E1);
+//            textview.setTextColor(0xff000000);
+            ImageView imageView=(ImageView) v;
             transaction = fragmentManager.beginTransaction();
             switch (v.getId()) {
                 case R.id.firstpage_frag:
+                    imageView.setImageResource(R.drawable.x102);
                     contentFragment = new FirstPageFragment();
                     transaction.replace(R.id.fragmentPager, contentFragment);
                     break;
                 case R.id.discover_frag:
+                    imageView.setImageResource(R.drawable.x202);
                     contentFragment = new DiscoverFragment();
                     transaction.replace(R.id.fragmentPager, contentFragment);
                     break;
                 case R.id.order_frag:
+                    imageView.setImageResource(R.drawable.x302);
                     contentFragment = new OrderFragment();
                     transaction.replace(R.id.fragmentPager, contentFragment);
                     break;
                 case R.id.personal_frag:
+                    imageView.setImageResource(R.drawable.x402);
                     contentFragment = new PersonalFragment();
                     transaction.replace(R.id.fragmentPager, contentFragment);
                     break;
